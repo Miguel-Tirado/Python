ea3be18d8c5a1667e709aa2fb7994e5fce1b775c
Miguel Tirado <miguel.e.tirado11@gmail.com>
2021-03-11 11:18:44 -0800

    added files about numbers

---
+++ python_work/favortie_number.py
@@ -0,0 +1,4 @@
+fav_num = 98
+print(f"My favorite number is {fav_num}.")
+# or
+print("My favorite number is " + str(fav_num) + '.')
---
+++ python_work/number_eight.py
@@ -0,0 +1,6 @@
+print(5 + 3)
+print(10 - 2)
+print(2 * 4)
+print(int(16 /2))
+# this is using exponet so 2 ^ 3 
+print(2**3)
