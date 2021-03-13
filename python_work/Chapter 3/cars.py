cff87aa607c6582d47a8f57479cd431ca8d6927e
Miguel Tirado <miguel.e.tirado11@gmail.com>
2021-03-12 16:38:04 -0800

    finished chapter 3

---
+++ python_work/Chapter 3/cars.py
@@ -0,0 +1,11 @@
+cars = ['bmw', 'audi','toyota','subaru']
+print("Here is the original list:")
+print(cars)
+print("\nHere is the sorted list:")
+print(sorted(cars))
+print("\nHere is the original list again")
+print(cars)
+print("\nHere is the reverse order")
+# the resverse method modifies the list permantly to start from the last itme on the list
+cars.reverse()
+print(cars)
