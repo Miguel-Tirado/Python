0bf16583890fa16f1cdd4f6d8fcb21f7d61f2d5c
Miguel Tirado <miguel.e.tirado11@gmail.com>
2021-03-15 16:53:04 -0700

    stuff

---
+++ python_work/Chapter_4/conditional_test.py
@@ -0,0 +1,12 @@
+car = 'tesla'
+print("is car == 'tesla'? I predict True.")
+print(car == 'tesla')
+
+print("\nIS car == 'audi'? I predict False")
+print(car == 'audi')
+
+print("\nIS car != 'audi'? I predict True")
+print(car != 'audi')
+
+print("\nIS car != 'tesla'? I predict False")
+print(car == 'tesla')
---
+++ python_work/Chapter_4/more_test.py
@@ -0,0 +1,31 @@
+fruit = ['apple','pineapple','orange','tomatoe']
+one_fruit = 'pear'
+
+print("is a 'pear' == orange? my guess is false")
+print(one_fruit == 'orange')
+
+print("\nis a 'pear' != orange? my guess is true")
+print(one_fruit != 'orange')
+
+num = 17
+print("\nis 17 == 18, my guess is false.")
+print(num == 18)
+print("\nis 17 != 18, my guess is true")
+print(num != 18)
+print("\nis 17 > 18, my guess is false.")
+print(num > 18)
+print("\nis 17 < 18, my guess is true.")
+print(num < 18)
+print("\nis 17 >= 18, my guess is false.")
+print(num >= 18)
+print("\nis 17 <= 18, my guess is true.")
+print(num <= 18)
+
+print("\nis 17 < 18 and pear == pear, my gueess is true")
+print(num < 18 and one_fruit == 'pear')
+
+print("\nis carrot a fruit? my guess is false")
+print('carrot' in fruit)
+
+print("\nis lettuce a fruit? my guess is false")
+print('lettuce' in fruit)
