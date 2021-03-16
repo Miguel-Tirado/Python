528e75e079963d1c07ce636231c5c9686f6de028
Miguel Tirado <miguel.e.tirado11@gmail.com>
2021-03-16 14:45:58 -0700

    Added files for chapter 5

---
+++ python_work/Chapter_5/favorite_fruits.py
@@ -0,0 +1,11 @@
+favorite_fruits = ['Pineapple','Mango','Orange']
+if 'pear' in favorite_fruits:
+    print("pears")
+if 'Pineapple' in favorite_fruits:
+    print("You really like pineapples!")
+if 'Mango' in favorite_fruits:
+    print("You really like Mangos!")
+if 'Orange' in favorite_fruits:
+    print("You really like Oranges!")
+if 'Apple' in favorite_fruits:
+    print("apples")
