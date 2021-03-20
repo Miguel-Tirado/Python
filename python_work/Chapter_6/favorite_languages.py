94e1929e51485efbaa73e675869da3d8e897614f
Miguel Tirado <miguel.e.tirado11@gmail.com>
2021-03-20 14:19:01 -0700

    added more files

--- python_work/Chapter_6/favorite_languages.py
+++ python_work/Chapter_6/favorite_languages.py
@@ -8,4 +8,19 @@ favorite_languages = {
 # to use the dictioary, given the name of a person who took the pool
 
 lanaguage = favorite_languages['sarah'].title()
-print(f"Sarah's favorite langauge is {lanaguage}.")
+print(f"Sarah's favorite langauge is {lanaguage}.\n")
+
+# adding a for loop to iterate over the dictionary 
+for name,langauge in favorite_languages.items():
+    print(f"{name.title()}'s favorite langauge is {lanaguage.title()}.")
+
+for name in favorite_languages.keys():
+    print(name.title())
+
+friends = ['phil','sarah']
+for name in favorite_languages.keys():
+    print(f"Hi {name.title()}.")
+
+    if name in friends:
+        language = favorite_languages[name].title()
+        print(f"\t{name.title()}, I see you love {language}!")
---
+++ python_work/Chapter_6/user.py
@@ -0,0 +1,10 @@
+user_o = {
+    'username' : 'efermi',
+    'first' : 'enrico',
+    'last' : 'fermo',
+}
+
+#note the items() method returns a list of key values pairs
+for key,value in user_o.items():
+    print(f"\nkey: {key}")
+    print(f"value: {value}")
