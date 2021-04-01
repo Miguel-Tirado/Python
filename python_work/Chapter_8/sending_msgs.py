13d4635b5be149f6463a6f6649badae9737194d0
Miguel Tirado <miguel.e.tirado11@gmail.com>
2021-04-01 09:54:41 -0700

    updated a file

--- python_work/Chapter_8/sending_msgs.py
+++ python_work/Chapter_8/sending_msgs.py
@@ -15,6 +15,5 @@ def send_messages(msgs):
 
 txt_msgs = ["how's your day.","what's up man","How was the event?","lets go somewhere"]
 
-send_messages(txt_msgs[:])
-print("-----------")
 show_messages(txt_msgs)
+send_messages(txt_msgs)
