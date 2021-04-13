86e95a4185e30c93fc6d597d4a22aafdb2181fea
Miguel Tirado <miguel.e.tirado11@gmail.com>
2021-04-12 19:10:13 -0700

    did exc's 15-1 to 15-2

--- Python_Projects/scatter_squares.py
+++ Python_Projects/scatter_squares.py
@@ -1,8 +1,13 @@
 import matplotlib.pyplot as plt 
 
+# x and y values stored inside list
+x_values = range(1,1001)
+y_values = [x**2 for x in x_values]
+
 plt.style.use('seaborn')
 fig, ax = plt.subplots()
-ax.scatter(2,4,s=200)
+#Note that c has y_values to know which values to set the colormap too
+ax.scatter(x_values,y_values,c=y_values, cmap=plt.cm.Blues, s=10)
 
 # set chart title and label axes.
 ax.set_title("Square Numbers", fontsize=24)
@@ -11,4 +16,7 @@ ax.set_ylabel("Sqaure of Value", fontsize=14)
 
 # set size of tick labels
 ax.tick_params(axis='both', which='major',labelsize=14)
-plt.show()
+ax.axis([0,1100, 0, 1100000])
+# save the output to a file
+plt.savefig('sqaures_plot.png', bbox_inches='tight')
+#plt.show()
